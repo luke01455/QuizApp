51d2bafec4e45119b2869372f60ff58da6ede3a7
luke01455 <luke0145555@hotmail.co.uk>
2019-12-06 20:34:36 +0000

    received question and rendered question on screen

--- src/components/quiz-modal/quiz-modal.component.jsx
+++ src/components/quiz-modal/quiz-modal.component.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './quiz-modal.styles.scss';
 
 const QuizModal = () => {
+    const [question, setQuestion] = useState([])
     const quizResults = useRef();
 
     const getContent = () => {
+        let allResults = [];
         const url = new URL('https://itunes.apple.com/search');
         const params = { term: 'beyonce', media: 'musicVideo'};
         url.search = new URLSearchParams(params);
         fetch(url, { method: 'POST' })
             .then(results => results.json())
             .then(data => {
-                return [
-                data.results[0].artworkUrl100,
-                data.results[1].trackCensoredName,
-                data.results[12].trackCensoredName,
-                data.results[23].trackCensoredName,
-                data.results[44].trackCensoredName
-            ];
-               
+                let getQuestion = () => {
+                    return ( <div> {data.results[12].trackCensoredName} </div> )
+                }
+                // allResults = [
+                // data.results[0].artworkUrl100,
+                // data.results[1].trackCensoredName,
+                // data.results[12].trackCensoredName,
+                // data.results[23].trackCensoredName,
+                // data.results[44].trackCensoredName
+            //];
+            setQuestion(getQuestion)
             })
     };
 
     useEffect(() => {
         const quizSetUp = getContent();
-        quizResults.current = quizSetUp;
-        console.log(quizResults.current)
+        //quizResults.current = quizSetUp;
+        console.log(quizSetUp)
     }, [])
 
     
@@ -34,7 +39,7 @@ const QuizModal = () => {
     return (
     <div className='styled-modal'>
         <div className='modal-container'>
-            <div className='modal-warning'> question
+            <div className='modal-warning'> {question}
             </div>
             <div className='modal-section-wrapper'>
 
