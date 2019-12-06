9f11fc47cd9849cb1115b8817c6713a347b1b2f0
luke01455 <luke0145555@hotmail.co.uk>
2019-12-06 19:38:10 +0000

    getting results from itunes api

--- src/components/quiz-modal/quiz-modal.component.jsx
+++ src/components/quiz-modal/quiz-modal.component.jsx
@@ -1,30 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import './quiz-modal.styles.scss';
 
 const QuizModal = () => {
+    const quizResults = useRef();
+
+    const getContent = () => {
+        const url = new URL('https://itunes.apple.com/search');
+        const params = { term: 'beyonce', media: 'musicVideo'};
+        url.search = new URLSearchParams(params);
+        fetch(url, { method: 'POST' })
+            .then(results => results.json())
+            .then(data => {
+                return [
+                data.results[0].artworkUrl100,
+                data.results[1].trackCensoredName,
+                data.results[12].trackCensoredName,
+                data.results[23].trackCensoredName,
+                data.results[44].trackCensoredName
+            ];
+               
+            })
+    };
+
     useEffect(() => {
-        const getContent = () => {
-            const url = new URL('https://itunes.apple.com/search');
-            const params = { term: 'beyonce', media: 'musicVideo'};
-            url.search = new URLSearchParams(params);
-            fetch(url, { method: 'POST' })
-                .then(results => results.json())
-                .then(data => {
-                    
-                })
-        };
+        const quizSetUp = getContent();
+        quizResults.current = quizSetUp;
+        console.log(quizResults.current)
     }, [])
+
+    
+
     return (
     <div className='styled-modal'>
         <div className='modal-container'>
             <div className='modal-warning'> question
             </div>
             <div className='modal-section-wrapper'>
-                answers
-                answers
-                answers
-                answers
+
             </div>
         </div>
     </div>
