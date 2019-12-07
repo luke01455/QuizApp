8ddd36606a592864961c489a9ffb56b178e84bf9
luke01455 <luke0145555@hotmail.co.uk>
2019-12-07 15:16:02 +0000

    added spinner, works for the loading components, but need it to work when loading from api also

--- src/App.js
+++ src/App.js
@@ -1,69 +1,27 @@
-import React from 'react';
+import React,  { lazy, Suspense } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.scss';
 
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Spinner from './components/spinner/spinner.component';
 
-import HomePage from './pages/homepage/homepage.component';
-import PrizeOption from './components/prize-option/prize-option.component';
-import QuizModal from './components/quiz-modal/quiz-modal.component';
-import QuizPage from './pages/quiz-page/quiz-page.component'
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const QuizModal = lazy(() => import('./components/quiz-modal/quiz-modal.component'));
+const QuizPage = lazy(() => import('./pages/quiz-page/quiz-page.component'));
 
 const App = () => {
-  const questions = [
-    {
-      question: '2x2',
-      imageUrl: 'https://i.ibb.co/8ztMxrV/repeat3img.jpg',
-      id: 1,
-      1: '4',
-      2: '11',
-      3: '245',
-      4: '555',
-      correct: 1
-    },
-    {
-      question: '3x3',
-      imageUrl: 'https://i.ibb.co/8ztMxrV/repeat3img.jpg',
-      id: 2,
-      1: '43',
-      2: '9',
-      3: '1',
-      4: '23',
-      correct: 2
-    },
-    {
-      question: '4x4',
-      imageUrl: 'https://i.ibb.co/8ztMxrV/repeat3img.jpg',
-      id: 3,
-      1: '432',
-      2: '234',
-      3: '423',
-      4: '16',
-      correct: 4
-    },
-    {
-      question: '5x5',
-      imageUrl: 'https://i.ibb.co/8ztMxrV/repeat3img.jpg',
-      id: 4,
-      1: '434',
-      2: '4344',
-      3: '25',
-      4: '4421',
-      correct: 3
-    },
-  ]
-
   return (
     <div className="App">
       <BrowserRouter>
-         
         <Switch>
-          <Route path="/quiz" component={QuizPage} />
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/beginquiz" component={QuizModal} />
+          <Suspense fallback={<Spinner />}>
+            <Route path="/quiz" component={QuizPage} />
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/beginquiz" component={QuizModal} />
+          </Suspense>
         </Switch>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
