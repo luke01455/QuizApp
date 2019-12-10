81d97800f5baf99cc2bdf2d477c89728375ae11c
luke01455 <luke0145555@hotmail.co.uk>
2019-12-10 17:32:47 +0000

    made quiz modal pop up instead of use routing in quiz-page

--- src/pages/quiz-page/quiz-page.component.jsx
+++ src/pages/quiz-page/quiz-page.component.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import './quiz-page.styles.scss';
-import { Link} from 'react-router-dom';
+
+import Spinner from '../../components/spinner/spinner.component';
+
+const QuizModal = lazy(() => import('../../components/quiz-modal/quiz-modal.component'));
+
+
 
 const QuizPage = () => {
     const strArr = window.location.href.split("/");
     const arrLength = strArr.length;
     const quizName = strArr[arrLength-1];
+    const [modalOnOff, setModalOnOff] = useState(false);
     return (
         <div className='quiz-page-container'>
             <div> Get all the questions right to win the {quizName}</div>
-            <Link as='button' to={`/beginquiz`}> START QUIZ </Link>
+            <button className='start-quiz-button' onClick={() => setModalOnOff(!modalOnOff)}> START QUIZ </button>
+            {
+                modalOnOff ? <Suspense fallback={<Spinner />}> <QuizModal /> </Suspense> : <div></div>
+            }
         </div>
     )
 }
