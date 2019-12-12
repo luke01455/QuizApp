5848d4c302330a38acea33df47a79e450cafaec6
luke01455 <luke0145555@hotmail.co.uk>
2019-12-12 15:49:05 +0000

    Updates

---
+++ precache-manifest.37d13ac9b54d860bc6772898dcccf9dd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4469e20b3d9f6423ebe4c91235ac5ce",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "dae689c1eb4db3765a24",
+    "url": "/quizapp/static/css/3.8eba592a.chunk.css"
+  },
+  {
+    "revision": "ddcfba407c30718afca6",
+    "url": "/quizapp/static/css/4.ca6b67d8.chunk.css"
+  },
+  {
+    "revision": "643dea294c2b814ac322",
+    "url": "/quizapp/static/css/5.9f58df32.chunk.css"
+  },
+  {
+    "revision": "4ea6c6d62968ea7eb620",
+    "url": "/quizapp/static/css/6.25cba087.chunk.css"
+  },
+  {
+    "revision": "073a4f119552cf58f4f3",
+    "url": "/quizapp/static/css/main.373a7e04.chunk.css"
+  },
+  {
+    "revision": "1694a6265fa8b214cfea",
+    "url": "/quizapp/static/js/2.bdc4eccc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/quizapp/static/js/2.bdc4eccc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dae689c1eb4db3765a24",
+    "url": "/quizapp/static/js/3.bdf235e6.chunk.js"
+  },
+  {
+    "revision": "ddcfba407c30718afca6",
+    "url": "/quizapp/static/js/4.04c692c5.chunk.js"
+  },
+  {
+    "revision": "643dea294c2b814ac322",
+    "url": "/quizapp/static/js/5.e94973c8.chunk.js"
+  },
+  {
+    "revision": "4ea6c6d62968ea7eb620",
+    "url": "/quizapp/static/js/6.9a68f039.chunk.js"
+  },
+  {
+    "revision": "073a4f119552cf58f4f3",
+    "url": "/quizapp/static/js/main.2f4dc6a3.chunk.js"
+  },
+  {
+    "revision": "ff93a152114e59aef57a",
+    "url": "/quizapp/static/js/runtime-main.3097d254.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/quizapp/static/media/crown.ed28accb.svg"
+  }
+]);
