552d2907486f2241bd7773eb2f90eabd57d10614
luke01455 <luke0145555@hotmail.co.uk>
2019-12-12 16:04:46 +0000

    changed router for gh pages temporarily

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Hashrouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 
 import  store from "./redux/store";
 
 ReactDOM.render(
     <Provider store={store}>
-    <BrowserRouter>
+    <Hashrouter>
         <App />
-    </BrowserRouter>
+    </Hashrouter>
   </Provider>,
    document.getElementById('root')
 );
