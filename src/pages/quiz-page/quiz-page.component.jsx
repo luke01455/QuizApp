ad5b072d3d25546a84bcafef3dc8427ced2278bf
luke01455 <luke0145555@hotmail.co.uk>
2020-01-07 18:48:33 +0000

    made link to redirect to login if trying to enter quiz and not a member

--- src/pages/quiz-page/quiz-page.component.jsx
+++ src/pages/quiz-page/quiz-page.component.jsx
@@ -1,7 +1,9 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense, useState, useContext } from 'react';
 import './quiz-page.styles.scss';
 import gql from 'graphql-tag'
 import { useMutation } from "@apollo/react-hooks";
+import { AuthContext } from "../../context/auth";
+import { Link } from 'react-router-dom'
 
 import Spinner from '../../components/spinner/spinner.component';
 
@@ -11,6 +13,7 @@ const QuizModalSport = lazy(() => import('../../components/quiz-modal-sport/quiz
 
 
 const QuizPage = (props) => {
+    const { user } = useContext(AuthContext);
     const strArr = window.location.href.split("/");
     const arrLength = strArr.length;
     const quizId = strArr[arrLength-2];
@@ -35,7 +38,12 @@ const QuizPage = (props) => {
      return (
         <div className='quiz-page-container'>
             <div> Get all the questions right to win the Prize</div>
-            <button className='start-quiz-button' onClick={() => onStart()}> START QUIZ </button>
+
+            {
+            user ? (<button className='start-quiz-button' onClick={() => onStart()}> START QUIZ </button>) : 
+            (<Link className='start-quiz-button' to='/signin'> START QUIZ </Link>)
+            }
+            
             {
                 modalOnOff ? <Suspense fallback={<Spinner />}> {quizTitle === 'MUSIC' ? <QuizModalMusic /> : <QuizModalSport />} </Suspense> : <div></div>
             }
