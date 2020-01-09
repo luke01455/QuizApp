c2e1de614d7fe524a2e85a84c8822b92da5ee511
luke01455 <luke0145555@hotmail.co.uk>
2020-01-09 19:37:30 +0000

    added some extra title to prize options

--- src/components/prize-option/prize-option.component.jsx
+++ src/components/prize-option/prize-option.component.jsx
@@ -4,18 +4,28 @@ import './prize-option.styles.scss';
 
 const PrizeOption = ({ title, imageUrl, size, history, id, userCount, maxUsers }) => (
   <div
-  onClick={() => history.push(`quiz/${title}/${id}`)}
-  className={`${size} menu-item`}>
+    onClick={() => history.push(`quiz/${title}/${id}`)}
+    className={`${size} menu-item`}>
+
     <div
       className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
-    />
+    >
+      <div className='quiz-title-top'       style={{
+        backgroundImage: `url(https://www.listenmoneymatters.com/wp-content/uploads/2018/10/FREE-Gift-Box-or-Card.jpg)`
+      }}> £250 AMAZON GIFT CARD </div>
+      <div className='quiz-title-bottom'> {title.toUpperCase()} </div>
+    </div>
+
+
     <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <h1 className='title'> ENTER </h1>
       <span className='subtitle'> {userCount} / {maxUsers} </span>
     </div>
+
+
   </div>
 );
 
