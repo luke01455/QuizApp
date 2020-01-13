bc0781a658562ac36d7330a143aa8a7f04447a6b
luke01455 <luke0145555@hotmail.co.uk>
2020-01-13 12:42:19 +0000

    edited images so the sizing is now better

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -23,39 +23,21 @@ const ControlledCarousel = () => {
             src={carouselimg1}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          {/* <div className='carousel-container'>
-          <div className='bground'> LEft side </div>
-          </div> */}
-  
         <img
             className="d-block w-100"
             src={carouselimg2}
-            alt="2nd slide"
+            alt="First slide"
           />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item >
-          <img
-            className="d-block w-99"
+        <img
+            className="d-block w-100"
             src={carouselimg3}
-            alt="Third slide"
+            alt="First slide"
           />
-  
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
