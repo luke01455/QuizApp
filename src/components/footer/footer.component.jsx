75693ef92ebe03ce4b059d59870fbe83a5d5f52d
luke01455 <luke0145555@hotmail.co.uk>
2020-01-15 18:53:53 +0000

    added some spacing and barebones to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,9 @@ import './footer.styles.scss'
 const Footer = () => {
     return (
         <div className='footer-container'>
-           <p className='footer-title'> Footer </p> 
+           <div className='footer-1'> FAQ's </div>
+           <div className='footer-2'> Newsletter </div>
+           <div className='footer-3'> Info </div>
         </div>
     );
 };
