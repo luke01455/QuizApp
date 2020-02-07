3607e4580bbb1f05ac9efdc9a5c7bd73d1b5f920
luke01455 <luke0145555@hotmail.co.uk>
2020-02-07 15:40:24 +0000

    added and edited some positioning of the new homescreen

--- src/components/homescreen/homescreen.component.jsx
+++ src/components/homescreen/homescreen.component.jsx
@@ -6,7 +6,7 @@ const HomeScreen = () => {
         <div className='homescreen-container'>
             <div className='text-container'>
                 <h1> Earn cash with your sport knowledge </h1>
-                <h1> All of those days sitting on the couch watching BT Sport can finally put to use. </h1>
+                <h2> All of those days sitting on the couch watching BT Sport can finally be put to use. </h2>
             </div>
         </div>
     )
