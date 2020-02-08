50afdd5acc938d22f48f954ee9e847f14278b182
luke01455 <luke0145555@hotmail.co.uk>
2020-02-08 19:35:01 +0000

    made a new card for the different quizzes

--- src/components/homescreen/homescreen.component.jsx
+++ src/components/homescreen/homescreen.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './homescreen.styles.scss';
 
+import QuizCard from '../quiz-card/quiz-card.component'
+
 const HomeScreen = () => {
     return (
         <div className='homescreen-container'>
@@ -8,6 +10,7 @@ const HomeScreen = () => {
                 <h1> Earn cash with your sport knowledge </h1>
                 <h2> All of those days sitting on the couch watching BT Sport can finally be put to use. </h2>
             </div>
+            <QuizCard />
         </div>
     )
 }
---
+++ src/components/quiz-card/quiz-card.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './quiz-card.styles.scss';
+
+const QuizCard = () => {
+    return (
+        <div className='card-container'>
+           <div className='prize-picture-container'></div>
+           <div className='info-container'></div>
+        </div>
+    )
+}
+
+export default QuizCard;
