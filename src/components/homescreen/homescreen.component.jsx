45a3050e743e72c9a0dfe87ed58e955ae2f91d35
luke01455 <luke0145555@hotmail.co.uk>
2020-02-10 18:56:43 +0000

    added instructions and some styling to home page

--- src/components/homescreen/homescreen.component.jsx
+++ src/components/homescreen/homescreen.component.jsx
@@ -10,7 +10,22 @@ const HomeScreen = () => {
                 <h1> Earn cash with your sport knowledge </h1>
                 <h2> All of those days sitting on the couch watching BT Sport can finally be put to use. </h2>
             </div>
-            <QuizCard />
+            {/* <QuizCard /> */}
+
+            <div className='instruction-container'>
+                <div className='instruction-section'>
+                    <h1> 1 </h1>
+                    <h3> Pick a quiz with a prize you'd like to win </h3>
+                </div>
+                <div className='instruction-section'>
+                    <h1> 2 </h1>
+                    <h3> Answer 5 simple questions like this </h3>
+                </div>
+                <div className='instruction-section'>
+                    <h1> 3 </h1>
+                    <h3> Each correct answer earns you an extra ticket increasing your chance to win </h3>
+                </div>
+            </div>
         </div>
     )
 }
