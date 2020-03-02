eb95658c52f71d9af3c47dd53eb40319e58d7b4c
luke01455 <luke0145555@hotmail.co.uk>
2020-03-02 11:02:51 +0000

    improved footer, made background image and added it

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,9 +4,19 @@ import './footer.styles.scss'
 const Footer = () => {
     return (
         <div className='footer-container'>
-           <div className='footer-1'> FAQ's </div>
-           <div className='footer-2'> Newsletter </div>
-           <div className='footer-3'> Info </div>
+            <div className='top-footer-section'>
+                <div className='footer-1'> FAQ's </div>
+                <div className='footer-2'> Newsletter </div>
+                <div className='footer-3'> Info </div>
+            </div>
+            <div className='socials-footer'>
+
+            </div>
+            <div className='lower-footer-section'>
+                <div> SITE INFO BLA </div>
+                <div> COPYRIGHT BLA BLA </div>
+                <div> TERMS AND CONDITIONS | PRIVACY | LEGAL NOTICE </div>
+            </div>
         </div>
     );
 };
