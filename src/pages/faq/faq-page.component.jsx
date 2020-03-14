2a8f29ca76b49e1bedffaca8579794ae324f9d89
luke01455 <luke0145555@hotmail.co.uk>
2020-03-14 20:23:09 +0000

    edited styling on faq page and added basic function to set state of which modal needs opening based on the question clicked

--- src/pages/faq/faq-page.component.jsx
+++ src/pages/faq/faq-page.component.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './faq-page.styles.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import Header from '../../components/header/header.component'
 
 const FaqPage = () => {
+    const [openQuestion, setOpenQuestion] = useState(0);
+
+    const modalOpen = (modalNum) => {
+        if (modalNum === openQuestion) {
+            setOpenQuestion(0)
+        } else {
+            setOpenQuestion(modalNum)
+        }
+    }
+
     return (
         <div className='faq-page-container'>
-            <Header transparency='nontransparent'/>
+            <Header transparency='nontransparent' />
             <div className='faq-page-wrapper'>
-            <h1> FREQUENTLY ASKED QUESTIONS </h1>
-            <p> How many times can I enter? </p>
-            <p> How will a winner be selected? </p>
-            <p> When does the competition close? </p>
-            <p> If I win how do i collect my prize? </p>
+                <h1> FREQUENTLY ASKED QUESTIONS </h1>
+
+                <p>If you have any questions regarding how the raffle works you can find most of your questions are already answered within our FAQ’s below.</p>
+                <div onClick={modalOpen(1)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> How many times can I enter? </h3> </div>
+                <div onClick={modalOpen(2)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> How will a winner be selected? </h3> </div>
+                <div onClick={modalOpen(3)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> When does the competition close? </h3> </div>
+                <div onClick={modalOpen(4)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> If I win how do i collect my prize? </h3> </div>
             </div>
         </div>
     )
