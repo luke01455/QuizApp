31e5c16ae9e1538097a7ea362e81801569ada8f8
luke01455 <luke0145555@hotmail.co.uk>
2020-03-16 12:43:11 +0000

    added transitions to FAQ page to make it flow better

--- src/pages/faq/faq-page.component.jsx
+++ src/pages/faq/faq-page.component.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import './faq-page.styles.scss';
 
+import {SlideDown} from 'react-slidedown'
+import 'react-slidedown/lib/slidedown.css'
+
+import Footer from '../../components/footer/footer.component'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,34 +17,41 @@ const FaqPage = () => {
     const modalOpen = (modalNum) => {
         if (modalNum === openQuestion) {
             setOpenQuestion(0)
-            console.log(openQuestion)
         } else {
             setOpenQuestion(modalNum)
-            console.log(openQuestion)
         }
     }
 
     return (
+        <SlideDown>
         <div className='faq-page-container'>
             <Header transparency='nontransparent' />
             <div className='faq-page-wrapper'>
                 <h1> FREQUENTLY ASKED QUESTIONS </h1>
 
                 <p>If you have any questions regarding how the raffle works you can find most of your questions are already answered within our FAQ’s below.</p>
-                <div onClick={() => modalOpen(1)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> How many times can I enter? </h3> </div>
-                { openQuestion === 1 && <p> You can enter each individual competition up to as many times as you want unless otherwise stated on the product page of each raffle.
+                <div onClick={() => modalOpen(1)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3 className='faq-question'> How many times can I enter? </h3> </div>
+                <div className={`transition-paragraph ${openQuestion == 1 ? 'show' : 'hide'}`}> 
+                    You can enter each individual competition up to as many times as you want unless otherwise stated on the product page of each raffle.
 
-Once all entries are in and the draw is closed, all entries will then be posted on our social media profiles and website within 72 hours. This will include your draw number to the right of your name which is what we will use in the LIVE draw to decide the winner.
+                    Once all entries are in and the draw is closed, all entries will then be posted on our social media profiles and website within 72 hours. This will include your draw number to the right of your name which is what we will use in the LIVE draw to decide the winner.
 
-This process allows you to watch the live draw with your corresponding raffle number(s) in hand. </p>}
-                <div onClick={() => modalOpen(2)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> How will a winner be selected? </h3> </div>
-                { openQuestion === 2 && <p> SportBounty's random number generator will be used to choose a winner, the name linking to the winning number chosen, will be the winner, this is all done automatically using algorithms with no human interferance or bias involved. </p>}
-                <div onClick={() => modalOpen(3)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> When does the competition close? </h3> </div>
-                { openQuestion === 3 && <p> Each prize raffle will be drawn around 30 minutes after the last perosn has entered. Information relating to the draw date of each raffle is visible on each quizzes page. </p>}
-                <div onClick={() => modalOpen(4)} className='faq-option'> <FontAwesomeIcon icon={faPlus} /> <h3> If I win how do i collect my prize? </h3> </div>
-                { openQuestion === 4 && <p>  All prizes will be sent via post once the competition has closed. Expect your prize within 7 working days! </p>}
+                    This process allows you to watch the live draw with your corresponding raffle number(s) in hand. 
+                </div>
+                <div onClick={() => modalOpen(2)} className={`faq-option`}> <FontAwesomeIcon icon={faPlus} /> <h3> How will a winner be selected? </h3> </div>
+                <div className={`transition-paragraph ${openQuestion == 2 ? 'show' : 'hide'}`}> SportBounty's random number generator will be used to choose a winner, the name linking to the winning number chosen, will be the winner, this is all done automatically using algorithms with no human interferance or bias involved. </div>
+                <div onClick={() => modalOpen(3)} className={`faq-option`}> <FontAwesomeIcon icon={faPlus} /> <h3> When does the competition close? </h3> </div>
+                <div className={`transition-paragraph ${openQuestion === 3 ? 'show' : 'hide'}`}> Each prize raffle will be drawn around 30 minutes after the last person has entered. Information relating to the draw date of each raffle is visible on each quizzes page. </div>
+                <SlideDown>
+                <div onClick={() => modalOpen(4)} className={`faq-option`}> <FontAwesomeIcon icon={faPlus} /> <h3> If I win how do i collect my prize? </h3> </div>
+                </SlideDown>
+                <div className={`transition-paragraph ${openQuestion === 4 ? 'show' : 'hide'}`}>  All prizes will be sent via post once the competition has closed. Expect your prize within 7 working days! </div>
             </div>
+            
         </div>
+        <Footer />
+        </SlideDown>
+        
     )
 }
 
