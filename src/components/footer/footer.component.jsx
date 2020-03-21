09a8ad8d0637e2f09599c5bf539d401cd8babba6
luke01455 <luke0145555@hotmail.co.uk>
2020-03-21 20:16:34 +0000

    added some links and images etc to footer to improve ux

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './footer.styles.scss'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHashtag } from '@fortawesome/free-solid-svg-icons'
 
 import twitterIcon from '../../assets/icons/bxl-twitter.svg'
 import facebookIcon from '../../assets/icons/bxl-facebook.svg'
@@ -14,20 +12,25 @@ const Footer = () => {
     return (
         <div className='footer-container'>
             <div className='top-footer-section'>
-                <div className='footer-1'> FAQ's </div>
-                <div className='footer-2'> NEWSLETTER </div>
-                <div className='footer-3'> INFO </div>
+                <Link to='/faqs' className='footer-link'> FAQ's </Link>
+                <Link to='/' className='footer-link'> NEWSLETTER </Link>
+                <Link to='/contact' className='footer-link'> INFO </Link>
             </div>
             <div className='socials-footer'>
-                <button className='footer-social-button'> <img src={twitterIcon}/> </button>
-                <button className='footer-social-button'> <img src={facebookIcon}/> </button>
-                <button className='footer-social-button'> <img src={githubIcon}/> </button>
-                <button className='footer-social-button'> <img src={instaIcon}/> </button>
+                <Link className='footer-socials-link' to='/'> <img className='social-icon' src={twitterIcon}/> </Link>
+                <Link className='footer-socials-link' to='/'> <img className='social-icon' src={facebookIcon}/> </Link>
+                <Link className='footer-socials-link' to='/'> <img className='social-icon' src={githubIcon}/> </Link>
+                <Link className='footer-socials-link' to='/'> <img className='social-icon' src={instaIcon}/> </Link>
             </div>
             <div className='lower-footer-section'>
-                <div> SITE INFO BLA </div>
-                <div> COPYRIGHT BLA BLA </div>
-                <div> TERMS AND CONDITIONS | PRIVACY | LEGAL NOTICE </div>
+                <div className='upper-footer-link-container'>
+                    <Link to='/' className='footer-link'> SITE INFO BLA </Link>
+                    <Link to='/' className='footer-link'> COPYRIGHT BLA BLA </Link>
+                </div>
+                <div className='lower-footer-link-container'> 
+                <Link to='/' className='footer-link'> TERMS AND CONDITIONS </Link> |  
+                <Link to='/' className='footer-link'> PRIVACY </Link> | 
+                <Link to='/' className='footer-link'> LEGAL NOTICE</Link> </div>
             </div>
         </div>
     );
