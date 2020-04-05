a17f07158c887b518ab5ef192eaee2cd6e822d74
luke john <luke0145555@hotmail.co.uk>
2020-04-05 16:31:43 +0100

    coped a very basic timer into quiz component, needs editing to suite needs but works for now

--- src/components/quiz-modal-sport/quiz-modal-sport.component.jsx
+++ src/components/quiz-modal-sport/quiz-modal-sport.component.jsx
@@ -203,6 +203,44 @@ const QuizModalSport = ({ scoreData }) => {
     }, []);
 
 
+        const calculateTimeLeft = () => {
+          const difference = +new Date("2021-01-01") - +new Date();
+          let timeLeft = {};
+      
+          if (difference > 0) {
+            timeLeft = {
+              days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+              hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+              minutes: Math.floor((difference / 1000 / 60) % 60),
+              seconds: Math.floor((difference / 1000) % 60)
+            };
+          }
+      
+          return timeLeft;
+        };
+
+        const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+        useEffect(() => {
+          setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+          }, 1000);
+        });
+      
+        const timerComponents = [];
+      
+        Object.keys(timeLeft).forEach(interval => {
+          if (!timeLeft[interval]) {
+            return;
+          }
+      
+          timerComponents.push(
+            <span>
+              {timeLeft[interval]} {interval}{" "}
+            </span>
+          );
+        });
+
     return (
         <div className='styled-modal'>
             {
@@ -212,6 +250,7 @@ const QuizModalSport = ({ scoreData }) => {
                     ) :
                     (
                         <div className='modal-container'>
+                            {timerComponents.length ? timerComponents : <span>Time's up!</span>}
                             <div className='question-header'> WHO IS THIS SPORTSPERSON? </div>
                             <div className='modal-question'> {question}
                             </div>
