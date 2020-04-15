c1a38896fc498853cee6193ad3da22307683cad1
luke john <luke0145555@hotmail.co.uk>
2020-04-15 17:18:02 +0100

    edited the quiz card a lot to match new design better, still work needed though

--- src/components/quiz-card/quiz-card.component.jsx
+++ src/components/quiz-card/quiz-card.component.jsx
@@ -6,12 +6,16 @@ const QuizCard = ({ size, title, userCount, maxUsers, price, id, prize, history 
 
     return (
         <div className={`card-container ${size}`} >
-           <div className='prize-picture-container'>
+           {/* <div className='prize-picture-container'>
                <div className='quiz-prize-name'> {prize ? prize : 'unknown'}  </div>
-           </div>
+           </div> */}
            <div className='info-container'>
-                <h3 className='top-text-container'> {title} </h3>
+                <h3 className='top-text-container'> {prize ? prize : 'unknown'} </h3>
+                <div className='prize-picture-container'>
+                        <img className='prize-picture' src="https://static.bigspark.com/shrine/8928/original-63aed47219.jpg"></img>
+                    </div>
                 <div className='lower-text-container'>
+
                     <p> £{price} </p> <p> {userCount}/{maxUsers} </p>
                 </div>
                 <Link className='enter-quiz-button' to={`quiz/${title}/${id}`}> See Details </Link>
