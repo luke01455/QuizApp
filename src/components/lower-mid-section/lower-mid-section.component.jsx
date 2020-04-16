009c73e3de0ff16df8376f1baf2b884110e1905a
luke john <luke0145555@hotmail.co.uk>
2020-04-16 17:02:26 +0100

    brought in fetch quizzes query into lower mid section

--- src/components/lower-mid-section/lower-mid-section.component.jsx
+++ src/components/lower-mid-section/lower-mid-section.component.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks'
 import './lower-mid-section.styles.scss';
 
+import { FETCH_QUIZZES_QUERY } from '../../util/graphql'
+
 import QuizCard from '../quiz-card/quiz-card.component'
-
-
+import Spinner from '../spinner/spinner.component'
+ 
 const LowerMidSection = () => {
+
+    const [quizzes, setQuizzes] = useState([]);
+    const { loading, data } = useQuery(FETCH_QUIZZES_QUERY)
+
+    useEffect(() => {
+        if (data) {
+            setQuizzes(data.getQuiz);
+        }
+    }, [data]);
+
     return (
         <div className='lower-mid-container'>
             <div className='quiz-card-container'> 
-            <QuizCard />
-            <QuizCard />
-            <QuizCard />
-            <QuizCard />
+                {loading ?
+                    (<Spinner />) :
+                    (
+                        quizzes &&
+                        quizzes.map(quiz => {
+                            return quiz.isActive === 'filling' &&
+                                (
+                                    <QuizCard
+                                        size='large'
+                                        key={quiz.id}
+                                        title={quiz.quizType === 'Music' ? 'Music' : 'Sport'}
+                                        userCount={quiz.userCount}
+                                        maxUsers={quiz.maxUsers}
+                                        price={quiz.price}
+                                        id={quiz.id}
+                                        prize={quiz.prize}
+                                    />
+                                )
+                        })
+                    )}
+
             </div>
         </div>
     )
