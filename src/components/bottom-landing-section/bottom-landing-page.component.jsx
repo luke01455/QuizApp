13ca9a63d7a3b8818c1d1b581398f7fdfb6d0ba7
luke john <luke0145555@hotmail.co.uk>
2020-04-20 18:42:14 +0100

    made the links on the homepage actually route correctly or open login modal depending on user status

--- src/components/bottom-landing-section/bottom-landing-page.component.jsx
+++ src/components/bottom-landing-section/bottom-landing-page.component.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'
 import './bottom-landing-page.styles.scss';
+
+import { AuthContext } from '../../context/auth'
 
 import CTAButton from '../cta-button/cta-button.component'
 
 const BottomLanding = () => {
+
+    const context = useContext(AuthContext)
 
     return (
         <div className='bottom-landing-container'>
@@ -11,9 +16,16 @@ const BottomLanding = () => {
                 <h1> TEST YOUR KNOWLEDGE </h1>
                 <h3> GET YOUR QUESTIONS  <br/> RIGHT TO WIN THE PRIZE </h3>
             </div>
-            <div className='bottom-landing-cta'>
-                <CTAButton> Try It Now </CTAButton>
-            </div>
+            {
+                context.user ? 
+                <Link className='underline-remove' to='/quizselect'>
+                    <div className='bottom-landing-cta'> <CTAButton> try it now </CTAButton> </div>
+                </Link>
+                 :
+                <Link className='underline-remove' onClick={() => context.loginModalToggle()}>
+                    <div className='bottom-landing-cta'> <CTAButton> try it now </CTAButton> </div>
+                </Link>
+            }
             
         </div>
     )
--- src/components/lower-mid-section/lower-mid-section.component.jsx
+++ src/components/lower-mid-section/lower-mid-section.component.jsx
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import './lower-mid-section.styles.scss';
+import { Link } from 'react-router-dom'
+import './lower-mid-section.styles.scss'
+
 
 import { FETCH_QUIZZES_QUERY } from '../../util/graphql'
+import { AuthContext } from '../../context/auth'
 
 import QuizCard from '../quiz-card/quiz-card.component'
 import Spinner from '../spinner/spinner.component'
 import CTAButton from '../cta-button/cta-button.component'
 
 const LowerMidSection = () => {
-
+    const context = useContext(AuthContext)
     const [quizzes, setQuizzes] = useState([]);
     const { loading, data } = useQuery(FETCH_QUIZZES_QUERY)
 
@@ -44,9 +47,16 @@ const LowerMidSection = () => {
                         })
                     )}
             </div>
-            <div className='lower-mid-cta'>
-                <CTAButton> Start Now </CTAButton>
-            </div>
+            {
+                context.user ? 
+                <Link className='underline-remove' to='/quizselect'>
+                    <div className='lower-mid-cta'> <CTAButton> start now </CTAButton> </div>
+                </Link>
+                 :
+                <Link className='underline-remove' onClick={() => context.loginModalToggle()}>
+                    <div className='lower-mid-cta'> <CTAButton> start now </CTAButton> </div>
+                </Link>
+            }
 
         </div>
     )
